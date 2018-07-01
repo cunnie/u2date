7b7cd80a90d34d6690dfa728f56b64e173d5d591
Brian Cunnie <brian.cunnie@gmail.com>
2018-07-01 12:36:49 -0700

    `cat`, again, but text files only, with bufio.Scanner

--- main/main.go
+++ main/main.go
@@ -5,30 +5,17 @@ import (
 	"github.com/cunnie/u2date/u2date"
 	"os"
 	"bufio"
-	"io"
 )
 
 func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	buffer := make([]byte, 64*1024) // 64 kiB, admittedly arbitrary
-
-	for {
-		n, err := stdin.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(fmt.Errorf("couldn't read stdin: %s", err.Error()))
-		}
-		stdout := bufio.NewWriter(os.Stdout)
-		o, err := stdout.Write(buffer[:n])
-		if err != nil {
-			panic(fmt.Errorf("couldn't write stdout: %s", err.Error()))
-		}
-		if (n != o) {
-			panic(fmt.Errorf("I expected to write %d, but instead wrote %d", n, o))
-		}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
 	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
 	os.Exit(0)
 	fmt.Println(u2date.U2date(os.Stdin))
 }
