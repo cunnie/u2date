551bdc03e1af66f1bc4d76914cbf066e3c01c7c6
Brian Cunnie <brian.cunnie@gmail.com>
2018-07-08 17:37:07 -0700

    Thorough integration tests
    - 2+ billion seconds since the UNIX Epoch
    - less than one billion seconds
    - no digit after the decimal point

--- u2date/u2date_test.go
+++ u2date/u2date_test.go
@@ -46,18 +46,19 @@ var _ = Describe("U2date", func() {
 
 	Describe("When passed a file with no carriage return on the last line", func() {
 		It("appends a carriage return (this is a mostly-harmless bug)", func() {
-			go writeToStdin(stdin, "a")
+			go writeToStdin(stdin, "a\nb\nc")
 			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Ω(err).ShouldNot(HaveOccurred())
-			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("a\n")))
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("a\nb\nc\n")))
 		})
 	})
 
-	Describe("When passed a file with a convertible time", func() {
+	Describe("When passed a file with a convertible time with no carriage return", func() {
 		It("converts it & appends a carriage return (this is a mostly-harmless bug)", func() {
-			go writeToStdin(stdin, "1500000000.0\n")
+			go writeToStdin(stdin, "1500000000.0")
 			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Ω(err).ShouldNot(HaveOccurred())
+			// Dmitriy & I were coding on BOSH-on-IPv6 when 1.5 billion seconds rolled
 			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT\n")))
 		})
 	})
@@ -67,10 +68,57 @@ var _ = Describe("U2date", func() {
 			go writeToStdin(stdin, "1500000000.0\n")
 			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 			Ω(err).ShouldNot(HaveOccurred())
-			// known bug: u2date will insert a "\n" when the timestamp is the very last
 			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT\n")))
 		})
 	})
+
+	Describe("When passed a file containing a timestamp that's 2 billion or greater", func() {
+		It("doesn't convert it", func() {
+			go writeToStdin(stdin, "2000000000.0")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2000000000.0\n")))
+		})
+	})
+
+	Describe("When passed a file containing a timestamp that's just shy of 2 billion", func() {
+		It("converts it", func() {
+			go writeToStdin(stdin, "1999999999.9")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2033-05-17 20:33:19.9 -0700 PDT\n")))
+		})
+	})
+
+	Describe("When passed a file containing a timestamp that's 1 billion", func() {
+		It("doesn't convert it", func() {
+			go writeToStdin(stdin, "1000000000.0")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2001-09-08 18:46:40 -0700 PDT\n")))
+		})
+	})
+
+	Describe("When passed a file containing a timestamp that's just shy of 1 billion", func() {
+		It("doesn't convert it", func() {
+			go writeToStdin(stdin, "999999999.9")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+			// known bug: u2date will insert a "\n" when the timestamp is the very last
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("999999999.9\n")))
+		})
+	})
+
+	Describe("When passed a file containing a timestamp that doesn't have a decimal point", func() {
+		It("doesn't convert it", func() {
+			go writeToStdin(stdin, "1500000000.0 1500000000. 1500000000")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Ω(err).ShouldNot(HaveOccurred())
+			// known bug: u2date will insert a "\n" when the timestamp is the very last
+			Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT 1500000000. 1500000000\n")))
+		})
+	})
+
 
 	AfterSuite(func() {
 		gexec.CleanupBuildArtifacts()
