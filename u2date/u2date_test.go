f401ef42bb6e95e2fb6d98168be0ef2459ce0104
Brian Cunnie <brian.cunnie@gmail.com>
2018-08-20 02:48:52 -0700

    Tests compaire strings, not `[]uint8`
    Makes reading tests easier; avoids cryptic output, e.g.:
    ```
    Expected
        <[]uint8 | len:32, cap:32>: [50, 48, 51, 51, 45, 48, 53, 45, 49, 55, 32, 50, 48, 58, 51, 51, 58, 49, 57, 46, 57, 32, 45, 48, 55, 48, 48, 32, 80, 68, 84, 10]
    to equal
    ```

--- u2date/u2date_test.go
+++ u2date/u2date_test.go
@@ -41,7 +41,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal(""))
 			})
 		})
 
@@ -50,7 +50,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "a\nb\nc")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("a\nb\nc\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("a\nb\nc\n"))
 			})
 		})
 
@@ -60,7 +60,7 @@ var _ = Describe("U2date", func() {
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
 				// Dmitriy & I were coding on BOSH-on-IPv6 when 1.5 billion seconds rolled
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2017-07-13 19:40:00 -0700 PDT\n"))
 			})
 		})
 	})
@@ -71,7 +71,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "1500000000.0\n")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2017-07-13 19:40:00 -0700 PDT\n"))
 			})
 		})
 
@@ -80,7 +80,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "2000000000.0")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2000000000.0\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2000000000.0\n"))
 			})
 		})
 
@@ -89,7 +89,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "11500000000.0\n")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("11500000000.0\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("11500000000.0\n"))
 			})
 		})
 		Describe("When passed a file containing a timestamp that's just shy of 2 billion", func() {
@@ -97,7 +97,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "1999999999.9")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2033-05-17 20:33:19.9 -0700 PDT\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2033-05-17 20:33:19.9 -0700 PDT\n"))
 			})
 		})
 
@@ -106,7 +106,7 @@ var _ = Describe("U2date", func() {
 				go writeToStdin(stdin, "1000000000.0")
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2001-09-08 18:46:40 -0700 PDT\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2001-09-08 18:46:40 -0700 PDT\n"))
 			})
 		})
 
@@ -116,7 +116,7 @@ var _ = Describe("U2date", func() {
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
 				// known bug: u2date will insert a "\n" when the timestamp is the very last
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("999999999.9\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("999999999.9\n"))
 			})
 		})
 
@@ -126,7 +126,7 @@ var _ = Describe("U2date", func() {
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Ω(err).ShouldNot(HaveOccurred())
 				// known bug: u2date will insert a "\n" when the timestamp is the very last
-				Ω(session.Wait().Out.Contents()).Should(Equal([]uint8("2017-07-13 19:40:00 -0700 PDT 1500000000. 1500000000\n")))
+				Ω(string(session.Wait().Out.Contents())).Should(Equal("2017-07-13 19:40:00 -0700 PDT 1500000000. 1500000000\n"))
 			})
 		})
 	})
