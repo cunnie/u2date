2c7113c1ffcffa173c623765b08d431db180ea00
Brian Cunnie <bcunnie@pivotal.io>
2019-12-14 15:58:00 -0800

    Better variable naming, useful comments
    And, in one case, better error checking.

--- u2date/main.go
+++ u2date/main.go
@@ -11,52 +11,52 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	input := bufio.NewScanner(os.Stdin)
 
-	reTimeSeconds := regexp.MustCompile(`([^\d]|^)1\d{9}\.\d+`)          // Seconds since the Epoch, includes subseconds
-	reTimeNanoseconds := regexp.MustCompile(`([^\d]|^)1\d{18}([^\d]|$)`) // Seconds since the Epoch, includes subseconds
+	reTimeSeconds := regexp.MustCompile(`([^\d]|^)1\d{9}\.\d+`)          // seconds since the Epoch, includes subseconds
+	reTimeNanoseconds := regexp.MustCompile(`([^\d]|^)1\d{18}([^\d]|$)`) // nanoseconds since the Epoch, no subseconds
 
-	for scanner.Scan() {
-		s := reTimeSeconds.ReplaceAllStringFunc(scanner.Text(), secondsToTime)
+	for input.Scan() {
+		s := reTimeSeconds.ReplaceAllStringFunc(input.Text(), secondsToTime)
 		s = reTimeNanoseconds.ReplaceAllStringFunc(s, nanosecondsToTime)
 		fmt.Println(s) // Println will add back the final '\n'
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input: %s", err.Error())
-	}
+	check(input.Err())
 }
 
-func secondsToTime(s string) string {
+func secondsToTime(rawSecondsString string) string {
+	// The regex might pick up an optional non-decimal leading character, "prefix"
 	prefix := ""
-	if s[0] != uint8('1') {
-		prefix = string(s[0])
-		s = s[1:]
+	if rawSecondsString[0] != uint8('1') {
+		prefix += string(rawSecondsString[0])
+		rawSecondsString = rawSecondsString[1:]
 	}
-	ses := strings.Split(s, ".")
-	seconds, err := strconv.ParseInt(ses[0], 10, 64)
+	secondsAndNanoseconds := strings.Split(rawSecondsString, ".")
+	seconds, err := strconv.ParseInt(secondsAndNanoseconds[0], 10, 64)
 	check(err)
-	nanoseconds, err := strconv.ParseInt((ses[1] + "000000000")[:9], 10, 64)
+	nanoseconds, err := strconv.ParseInt((secondsAndNanoseconds[1] + "000000000")[:9], 10, 64)
 	check(err)
 	return prefix + time.Unix(seconds, nanoseconds).String()
 }
 
-// static variable, declaring at global scope seems like the least evail
+// static variable, declaring at global scope seems like the least evil
 // https://stackoverflow.com/questions/30558071/static-local-variable-in-go
 var reDigits = regexp.MustCompile(`\d`)
 
-func nanosecondsToTime(s string) string {
+func nanosecondsToTime(rawNanosecondsString string) string {
+	// The regex might pick up optional non-decimal leading & trailing characters, "prefix" and "suffix"
 	prefix := ""
 	suffix := ""
-	if s[0] != uint8('1') {
-		prefix = string(s[0])
-		s = s[1:]
+	if rawNanosecondsString[0] != uint8('1') {
+		prefix = string(rawNanosecondsString[0])
+		rawNanosecondsString = rawNanosecondsString[1:]
 	}
-	a := reDigits.Find([]uint8(string(s[len(s)-1])))
-	if a == nil {
-		suffix = string(s[len(s)-1])
-		s = s[:len(s)-1]
+	lastCharIfDecimal := reDigits.Find([]uint8(string(rawNanosecondsString[len(rawNanosecondsString)-1])))
+	if lastCharIfDecimal == nil {
+		suffix = string(rawNanosecondsString[len(rawNanosecondsString)-1])
+		rawNanosecondsString = rawNanosecondsString[:len(rawNanosecondsString)-1]
 	}
-	nanoseconds, err := strconv.ParseInt(s, 10, 64)
+	nanoseconds, err := strconv.ParseInt(rawNanosecondsString, 10, 64)
 	check(err)
 	return prefix + time.Unix(0, nanoseconds).String() + suffix
 }
